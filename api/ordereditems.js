d409a648158d167016e190f18629626950b2b912
almogShenkar <almogassu@gmail.com>
2018-06-16 14:25:49 +0300

    names

--- api/ordereditems.js
+++ api/ordereditems.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 
 //Controllers
-var ordereditemsController = require('../controllers/ordereditems');
+var orderitemController = require('../controllers/ordereditems');
    
     
 
@@ -11,17 +11,17 @@ var ordereditemsController = require('../controllers/ordereditems');
 // --- Ordereditems API---//
 
 //localhost:3000/api/ordereditems
-router.get('/', ordereditemsController.list);
+router.get('/', orderitemController.list);
 //localhost:3000/api/ordereditems/2
-router.get('/:id', ordereditemsController.get);
+router.get('/:id', orderitemController.get);
 //localhost:3000/api/ordereditems/olid/5
-router.get('/olid/:id', ordereditemsController.listByOlid);
+router.get('/olid/:id', orderitemController.listByOlid);
 //localhost:3000/api/ordereditems
-router.put('/',ordereditemsController.update);
+router.put('/',orderitemController.update);
 //localhost:3000/api/ordereditems
-router.post('/', ordereditemsController.add);
+router.post('/', orderitemController.add);
 //localhost:3000/api/ordereditems/6
-router.delete('/:id', ordereditemsController.delete);
+router.delete('/:id', orderitemController.delete);
 
 
 module.exports = router;
