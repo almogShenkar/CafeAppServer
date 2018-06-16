bcc63bcc2e13c2be808547a22d94bbaf38cb6ce9
almogShenkar <almogassu@gmail.com>
2018-06-16 20:57:20 +0300

    db config,server bugs fix

--- db.js
+++ db.js
@@ -1,30 +1,31 @@
 
 var mysql = require('mysql');
-var connection = mysql.createConnection({
-        //dev env
-        //host: "localhost",
-        //adam  port: "8889",
-        //user: "root",
-        //prod env
-        host:"us-cdbr-iron-east-04.cleardb.net",
-        user:"b6295e767bfea4",
-        password:"4049d2f6",
-        database: "heroku_ee97203c1d832f5",
-        timezone: 'utc'  //<-here this line was missing
-        
-      });
 
+var pool = mysql.createPool({
+  //dev env
+  //host: "localhost",
+  //adam  port: "8889",
+  //user: "root",
+  //prod env
+  connectionLimit: 10,
+  host: "us-cdbr-iron-east-04.cleardb.net",
+  user: "b6295e767bfea4",
+  password: "4049d2f6",
+  database: "heroku_ee97203c1d832f5",
+  timezone: 'utc'  //<-here this line was missing
 
-connection.connect(function(err){
-    if(err){
-      console.log('Error connecting to Db');
-      return;
-    }
-    console.log('Connection established');
-  });
+});
 
-  
+pool.on('acquire', function (connection) {
+  console.log('Connection %d acquired', connection.threadId);
+});
 
+pool.on('connection', function (connection) {
+  console.log("Connected to db");
+});
 
-module.exports = connection;
+pool.on('release', function (connection) {
+  console.log('Connection %d released', connection.threadId);
+});
+module.exports = pool;
 
